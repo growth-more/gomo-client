529146bffa2a187434d4062e243a4d1e2442efd7
pokycookie <0cookieboy0@gmail.com>
2025-02-21 17:08:17 +0900

    feat: iconify 컴포넌트 속성 추가

--- src/components/iconify/iconify.tsx
+++ src/components/iconify/iconify.tsx
@@ -7,10 +7,10 @@ interface IconifyProps extends IconifyIconProps {
   sx?: SxProps<Theme>
 }
 
-export function Iconify({ width, icon, sx }: IconifyProps) {
+export function Iconify({ width, icon, sx, ...others }: IconifyProps) {
   return (
-    <Box sx={sx}>
-      <Icon icon={icon} width={width ?? 20} height={width ?? 20} />
+    <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', ...sx }}>
+      <Icon icon={icon} width={width ?? 20} height={width ?? 20} color={others.color} />
     </Box>
   )
 }
