6233ac95fd23b1b5a632ab9ee8cf8c21e56f240b
pokycookie <0cookieboy0@gmail.com>
2025-04-05 02:05:16 +0900

    feat: toast 알림 추상화

--- src/components/toast/toaster.tsx
+++ src/components/toast/toaster.tsx
@@ -1,10 +1,15 @@
 import { Toaster as SonnerToaster } from 'sonner'
 
-const TOAST_DURATION = 1500
+const TOAST_DURATION = 2000
 const TOAST_LIMIT = 3
 
 export function Toaster() {
   return (
-    <SonnerToaster position="top-right" duration={TOAST_DURATION} visibleToasts={TOAST_LIMIT} />
+    <SonnerToaster
+      position="top-right"
+      duration={TOAST_DURATION}
+      visibleToasts={TOAST_LIMIT}
+      richColors
+    />
   )
 }
