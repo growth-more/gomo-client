623b9db47a6ef0139ecb416e5bb3332dcc83ee84
pokycookie <0cookieboy0@gmail.com>
2025-07-11 17:25:01 +0900

    feat: 단일 기록 조회 api 정의 및 index  분리

--- src/api/hooks/streak/index.ts
+++ src/api/hooks/streak/index.ts
@@ -5,8 +5,8 @@ export * from './use-get-streak'
 export * from './use-get-achievement'
 
 export function useStreak(startDate: Date, endDate: Date) {
-  const { streak, isLoading } = useGetStreak(startDate, endDate)
-  const { achievement } = useGetAchievement()
+  const { streak, isLoading: streakLoading } = useGetStreak(startDate, endDate)
+  const { achievement, isLoading: achievementLoading } = useGetAchievement()
 
-  return { streak, achievement, isLoading }
+  return { streak, achievement, streakLoading, achievementLoading }
 }
